4f1b655b3e77aedf7335aadf7c1521c4c88c1689
Chuqian Yin <yinchuqian@D-69-91-128-135.dhcp4.washington.edu>
2018-04-19 21:31:59 -0700

    Practice more about dplyr in class

--- exercise-7/exercise.R
+++ exercise-7/exercise.R
@@ -1,44 +1,51 @@
 # Exercise 7: using dplyr on external data
 
 # Load the dplyr library
-
+library(dplyr)
 
 # Use the `read.csv()` function to read in the included data set. Remember to
 # save it as a variable.
-
+nba_teams <- read.csv(file = "data/nba_teams_2016.csv", stringsAsFactors = FALSE)
 
 # View the data frame you loaded, and get some basic information about the 
 # number of rows/columns. 
 # Note the "X" preceding some of the column titles as well as the "*" following
 # the names of teams that made it to the playoffs that year.
-
-
+View(nba_teams)
+nrow(nba_teams)
+ncol(nba_teams)
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
-
+nba_teams <- mutate(nba_teams, steal_ratio = TOV / STL)
 
 # Sort the teams from lowest turnover/steal ratio to highest
 # Which team has the lowest turnover/steal ratio?
+nba_teams <- arrange(nba_teams, steal_ratio)
 
+lowest_steal <- nba_teams %>% 
+  filter(steal_ratio == min(steal_ratio)) %>% 
+  select(Team)
 
 # Using the pipe operator, create a new column of assists per game (AST / G) 
 # AND sort the data.frame by this new column in descending order.
-
+nba_teams <- nba_teams %>% 
+  mutate(assist_per_game = AST / G) %>% 
+  arrange(-assist_per_game)
 
 # Create a data frame called `good_offense` of teams that scored more than 
 # 8700 points (PTS) in the season
-
+good_offense <- filter(nba_teams, PTS > 8700)
 
 # Create a data frame called `good_defense` of teams that had more than 
 # 470 blocks (BLK)
-
+good_defense <- filter(nba_teams, BLK > 470)
 
 # Create a data frame called `offense_stats` that only shows offensive 
 # rebounds (ORB), field-goal % (FG.), and assists (AST) along with the team name.
-
+offense_stats <- select(nba_teams, ORB, FG. , AST, Team)
 
 # Create a data frame called `defense_stats` that only shows defensive 
 # rebounds (DRB), steals (STL), and blocks (BLK) along with the team name.
-
+defense_stats <- select(nba_teams, DRB, STL, BLK, Team)
 
 # Create a function called `better_shooters` that takes in two teams and returns
 # a data frame of the team with the better field-goal percentage. Include the 
